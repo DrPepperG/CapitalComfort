c6bb15597a5f8aa48a0ca176ee3a996ebac4596c
DrPepper <josephcrain14@gmail.com>
2022-10-30 13:33:34 -0400

    dynamic favicon

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+    const { getThumbnail: img } = useDirectusFiles();
     const { $settings } = useNuxtApp()
 
     useHead({
+        link: [
+            { rel: 'icon', type: 'image/png', href: img($settings.site_logo, { format: 'png' }) }
+        ],
         titleTemplate: (title) => { return title ? `${title} | ${$settings.site_name}` : `${$settings.site_name}` }
     })
 </script>
