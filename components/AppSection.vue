2c8983f097309c2640884b22a36d22afb8968a2b
DrPepper <josephcrain14@gmail.com>
2022-11-14 20:11:34 -0500

    fix silly padding

--- components/AppSection.vue
+++ components/AppSection.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="mx-auto container mt-4 p-6">
-        <h1 class="text-6xl font-bold text-left uppercase mb-2">
-            <slot name="title" />
-        </h1>
-        <h2 class="font-semibold italic text-gray-700 mb-2">
-            <slot name="description" />
-        </h2>
-        <slot />
+    <div class="p-6">
+        <div class="mx-auto container">
+            <h1 class="text-6xl font-bold text-left uppercase mb-2">
+                <slot name="title" />
+            </h1>
+            <h2 class="font-semibold italic text-gray-700 mb-2">
+                <slot name="description" />
+            </h2>
+            <slot />
+        </div>
     </div>
 </template>
