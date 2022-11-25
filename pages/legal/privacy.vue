9abf61dfd7405093fabb2707105ef5876c340719
DrPepper <josephcrain14@gmail.com>
2022-11-25 12:07:00 -0500

    change privacy policy

--- pages/legal/privacy.vue
+++ pages/legal/privacy.vue
@@ -12,14 +12,16 @@
                 <h2 class="font-bold">Effective 11/13/2022</h2>
                 <div class="flex flex-col gap-4 mt-4">
                     <p>
-                        This site is provided by <span class="font-bold">Capital Comfort, LLC</span> and by visiting this site you agree to the following terms
+                        This Privacy Policy describes how <span class="font-bold">Capital Comfort, LLC</span> (the "Site" or "we") collects, uses, discloses your Personal Information when you visit our Site.
                     </p>
                     <p class="font-semibold italic">
                         This site is intended for users inside of the United States of America only.
                     </p>
                     <p>
-                        To provide us with user engagement data we enlist the help of Google Analytics, user and event data will be stored for <span class="font-bold">14 months</span>.
-                        If you would like more information on how Google processes your data please vist their <a class="text-cc-blue font-semibold" href="https://policies.google.com/technologies/partner-sites" target="_blank">Policy Page</a>.
+                        We use Google Analytics to help us understand how our clients use the Site. You can read more about how Google uses your Personal Information here: 
+                        <a class="text-cc-blue font-semibold" href="https://www.google.com/intl/en/policies/privacy/" target="_blank">https://www.google.com/intl/en/policies/privacy/</a>. 
+                        You can also opt-out of Google Analytics here:
+                        <a class="text-cc-blue font-semibold" href="https://tools.google.com/dlpage/gaoptout" target="_blank">https://tools.google.com/dlpage/gaoptout</a>.
                     </p>
                 </div>
             </AppSegment>
