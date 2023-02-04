f31c6adcfb5f6fe75d158c51562ac31a1989c40b
DrPepper <josephcrain14@gmail.com>
2023-02-03 22:14:10 -0500

    add an actual static favicon

--- app.vue
+++ app.vue
@@ -24,9 +24,17 @@
             { property: 'twitter:title', content: $settings.site_name },
             { property: 'twitter:description', content: $settings.site_description },
             { property: 'twitter:image', content: img($settings.site_image) },
+
+            // Favicon
+            { name: 'msapplication-TileColor', content: '#da532c'},
+            { name: 'theme-color', content: '#ffffff'}
         ],
         link: [
-            { rel: 'icon', type: 'image/png', href: img($settings.site_favicon, { format: 'png' }) }
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'},
+            { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
+            { rel: 'manifest', href: '/site.webmanifest' },
+            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
         ]
     })
 </script>
